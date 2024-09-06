d57ebb5741ddabd4dd5e68cd064b47c986f8f2d5
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2024-09-06 22:59:22 +0300

    ex

---
+++ 4/4-1/8.py
@@ -0,0 +1,9 @@
+x = int(input())
+if x <= 13:
+    print("детство")
+elif x <= 24 and x >= 14:
+    print("молодость")
+elif x <= 59 and x >= 25:
+    print("зрелость")
+elif x >= 60:
+    print("старость")
