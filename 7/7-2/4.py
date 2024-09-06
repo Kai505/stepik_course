a3f04fb77c64b8a27ab1e7855acbb67fc5eeadf6
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2024-09-06 23:19:37 +0300

    ex

---
+++ 7/7-2/4.py
@@ -0,0 +1,9 @@
+x = int(input())
+y = int(input())
+for i in range(x, y + 1):
+    if i % 17 == 0:
+        print(i)
+    elif i % 10 == 9:
+        print(i)
+    elif i % 3 == 0 and i % 5 == 0:
+        print(i)
