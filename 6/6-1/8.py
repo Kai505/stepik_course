376d7b9683fdd9a96b5e6532e71da636bc9157e7
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2024-09-06 23:06:59 +0300

    ex

---
+++ 6/6-1/8.py
@@ -0,0 +1,3 @@
+a, b, c, d, f = int(input()), int(input()), int(input()), int(input()), int(input())
+print("Наименьшее число =", min(a, b, c, d, f))
+print("Наибольшее число =", max(a, b, c, d, f))
